216dd61e10cc178da729ae6346d3ab98fa32efdb
Kryss <kryssbm99@gmail.com>
2024-03-25 10:37:49 -0400

    Creating printf

---
+++ README.md
@@ -0,0 +1,2 @@
+Welcome to our first group proyect!
+Memberes are Kryss Babilonia and Angel Velazquez
---
+++ main.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int _printf(const char *format, ...)
