d095b2ff862cfa75aebeffbeb1c696586edc12b7
AngelVelazq <angelvelazquez.op@gmail.com>
2024-03-27 13:59:08 -0400

    more fixes

--- main.h
+++ main.h
@@ -2,11 +2,25 @@
 #define MAIN_H_
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 #include <limits.h>
 
-int _printf(const char *format, ...)
+int  _printf(const char * const format, ...);
 int _putchar(char c);
-int _puts(char *str);
-int _printf(const char *format, ...);
+int printchar(va_list val);
+int printstring(va_list val);
+int printint(va_list val);
+int printpercent(va_list val);
 
+/**
+ *struct type - Struct function
+ *@b: Char variable for b
+ *@p: Pointer of p
+ *
+ */
+typedef struct functions
+{
+	char b;
+	int(*p)(va_list);
+} functions;
 #endif
